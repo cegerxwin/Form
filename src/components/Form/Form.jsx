e88704a0544b46c6291adc2bf3340603dc8b4140
Murat Oztemel <cegerxwin@gmail.com>
2024-03-08 09:57:56 +0300

    Add Additional Form Item

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -7,13 +7,17 @@ const initialState = {
   inputName: "",
   inputEmail: "",
   inputPassword: "",
+  inputBirthDate: "",
+  inputinputPhoneNumber: "",
+  inputAddress: "",
 };
 
 function Form() {
   const [customerInput, setCustomerInput] = useState(initialState);
-
   const [customerData, setCustomerData] = useState([]);
-
+  const [birthVisible, setBirthVisible] = useState(false);
+  const [phoneVisible, setPhoneVisible] = useState(false);
+  const [addressVisible, setAddressVisible] = useState(false);
   const formRef = useRef();
 
   useEffect(() => {
@@ -66,7 +70,114 @@ function Form() {
                 onChange={handleChange}
               />
             ))}
-            <fieldset className="relative z-0 w-full p-px">
+            <div className="relative z-0 w-full mb-5">
+              <h3 className="mb-4 font-semibold text-gray-900 dark:text-white">
+                Do you want to add extra information?
+              </h3>
+              <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                  <div className="flex items-center ps-3">
+                    <input
+                      id="vue-checkbox-list"
+                      type="checkbox"
+                      value=""
+                      onClick={(e) => setBirthVisible(e.target.checked)}
+                      checked={birthVisible}
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                    />
+                    <span
+                      htmlFor="vue-checkbox-list"
+                      className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                      Birth Date
+                    </span>
+                  </div>
+                </li>
+                <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                  <div className="flex items-center ps-3">
+                    <input
+                      id="react-checkbox-list"
+                      type="checkbox"
+                      value=""
+                      onClick={(e) => setPhoneVisible(e.target.checked)}
+                      checked={phoneVisible}
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                    />
+                    <span
+                      htmlFor="react-checkbox-list"
+                      className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                      Phone Number
+                    </span>
+                  </div>
+                </li>
+                <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                  <div className="flex items-center ps-3">
+                    <input
+                      id="angular-checkbox-list"
+                      type="checkbox"
+                      value=""
+                      onClick={(e) => setAddressVisible(e.target.checked)}
+                      checked={addressVisible}
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                    />
+                    <span
+                      htmlFor="angular-checkbox-list"
+                      className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                      Address
+                    </span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            {birthVisible && (
+              <div className="relative z-0 w-full mb-5">
+                <input
+                  type="number"
+                  name="inputBirthDate"
+                  className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+                  placeholder=" "
+                  onChange={handleChange}
+                />
+                <label
+                  htmlFor="birthDate"
+                  className="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">
+                  Birth Date
+                </label>
+              </div>
+            )}
+            {phoneVisible && (
+              <div className="relative z-0 w-full mb-5">
+                <input
+                  type="number"
+                  name="inputPhoneNumber"
+                  className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+                  placeholder=" "
+                  onChange={handleChange}
+                />
+                <label
+                  htmlFor="phoneNumber"
+                  className="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">
+                  Phone Number
+                </label>
+              </div>
+            )}
+            {addressVisible && (
+              <div className="relative z-0 w-full mb-5">
+                <textarea
+                  type="text"
+                  name="inputAddress"
+                  className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+                  placeholder=" "
+                  onChange={handleChange}
+                />
+                <label
+                  htmlFor="address"
+                  className="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">
+                  Address
+                </label>
+              </div>
+            )}
+
+            {/*             <fieldset className="relative z-0 w-full p-px">
               <legend className="absolute text-gray-500 transform scale-75 -top-3 origin-0">
                 Gender Identity
               </legend>
@@ -108,7 +219,7 @@ function Form() {
                   Prefer not to say
                 </label>
               </div>
-            </fieldset>
+            </fieldset> */}
           </div>
           <p className="text-xs text-red-500 text-right my-3">
             Required fields are marked with an asterisk{" "}
